34612343a3e79aa1c7481d18aa3baee4808ffdf3
Carlos Zamboni <carloszambrozio@gmail.com>
2026-02-06 15:02:49 -0300

    Atualizaçao total do projeto com ansible, grafana e prometheus

--- app/main.py
+++ app/main.py
@@ -1,19 +1,43 @@
-import os
 from fastapi import FastAPI
+from prometheus_client import Counter, Histogram, generate_latest
+from starlette.responses import Response
+import time
 
 app = FastAPI()
 
-APP_NAME = os.getenv("APP_NAME", "app-default")
-APP_ENV = os.getenv("APP_ENV", "local")
-
-@app.get("/")
-def root():
-    return {
-        "service": APP_NAME,
-        "env": APP_ENV,
-        "status": "running v2"
-    }
 
 @app.get("/health")
 def health():
     return {"status": "ok"}
+
+REQUEST_COUNT = Counter(
+    "http_requests_total",
+    "Total de requisições HTTP",
+    ["method", "endpoint"]
+)
+
+REQUEST_LATENCY = Histogram(
+    "http_request_latency_seconds",
+    "Latência das requisições"
+)
+
+@app.middleware("http")
+async def prometheus_middleware(request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    latency = time.time() - start_time
+
+    REQUEST_COUNT.labels(
+        method=request.method,
+        endpoint=request.url.path
+    ).inc()
+
+    REQUEST_LATENCY.observe(latency)
+    return response
+
+@app.get("/metrics")
+def metrics():
+    return Response(
+        generate_latest(),
+        media_type="text/plain"
+    )
